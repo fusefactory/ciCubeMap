8e07be541d68aff1acab3ef28bd045806775769f
mark <mark.korput@gmail.com>
2017-12-02 14:15:28 +0100

    add a failing test

--- tests/src/main.cpp
+++ tests/src/main.cpp
@@ -9,10 +9,24 @@
 
 using namespace std;
 using namespace ci;
-using namespace ci::CubeMap;
+// using namespace ci::CubeMap;
 
 TEST_CASE("Mapper", ""){
-  SECTION("Mapper::create"){
-    REQUIRE("TODO" == "TODO");
+  SECTION("Mapper::create initializes like an FboCubeMap"){
+
+    // TODO; initialize OpenGL context first
+
+    auto mapperRef = CubeMap::Mapper::create();
+    REQUIRE(mapperRef->getFboCubeMap()->getWidth() == 1024);
+    REQUIRE(mapperRef->getFboCubeMap()->getHeight() == 1024);
+
+    mapperRef = CubeMap::Mapper::create(2048, 2048);
+    REQUIRE(mapperRef->getFboCubeMap()->getWidth() == 2048);
+    REQUIRE(mapperRef->getFboCubeMap()->getHeight() == 2048);
+
+    auto fmt = ci::gl::FboCubeMap::Format();
+    mapperRef = CubeMap::Mapper::create(512, 512, fmt);
+    REQUIRE(mapperRef->getFboCubeMap()->getWidth() == 512);
+    REQUIRE(mapperRef->getFboCubeMap()->getHeight() == 512);
   }
 }
